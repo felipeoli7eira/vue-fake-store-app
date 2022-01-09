2dadd9e80ae2c9bcaa707e9290127ed90ba29d2b
felipeoli7eira <felipe.oliveira@wapstore.com.br>
2022-01-09 16:22:25 -0300

    removendo dependencia do vue-cookies

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import router from '@/router'
-import VueCookies from 'vue-cookies'
 
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
@@ -15,10 +14,8 @@ import './plugins/axios'
 
 // uses
 Vue.use(VueRouter)
-Vue.use(VueCookies)
 
 // configs
-Vue.$cookies.config('1d', '', '/', false, 'Lax')
 Vue.config.productionTip = false
 
 // Vue
